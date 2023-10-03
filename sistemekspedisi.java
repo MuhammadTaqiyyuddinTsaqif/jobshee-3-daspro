b75c263e236eb5bc671a99ff47cf73eac94789f1
muathoillah <140579954+muathoillah@users.noreply.github.com>
2023-10-03 12:27:07 +0700

    pro 2

--- sistemekspedisi.java
+++ sistemekspedisi.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class sistemekspedisi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         String nama_penerima, alamat_penerima, no_hp_penerima, nama_pengirim, alamat_pengirim, no_hp_pengirim, asal_pengiriman, tujuan_pengiriman;
         double harga, panjang, berat,tinggi, lebar,jarak;
         
+        System.out.print("input nama pengirim : ");
+        nama_penerima = sc.nextLine();
+        System.out.print("input alamat pengirim : ");
+        alamat_pengirim =sc.nextLine();
+        System.out.print("input no pengirim : ");
+        no_hp_pengirim =sc.nextLine();
+        System.out.print("input nama penerima : ");
+        nama_penerima =sc.nextLine();
+        System.out.print("input alamat penerima : ");
+        alamat_penerima =sc.nextLine();
+        System.out.print("input no penerima : ");
+        no_hp_penerima = sc.nextLine();
 }
 }
