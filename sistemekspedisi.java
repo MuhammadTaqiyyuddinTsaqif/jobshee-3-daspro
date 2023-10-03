6e5c51ada4f5144313d38ee7a6f5fff35ae857af
MuhammadTaqiyyuddinTsaqif <144300533+MuhammadTaqiyyuddinTsaqif@users.noreply.github.com>
2023-10-03 19:16:39 +0700

    update (tsaqif)

--- sistemekspedisi.java
+++ sistemekspedisi.java
@@ -5,7 +5,7 @@ public class sistemekspedisi {
         Scanner sc = new Scanner(System.in);
         
         String nama_penerima, alamat_penerima, no_hp_penerima, nama_pengirim, alamat_pengirim, no_hp_pengirim, asal_kota_pengiriman, kota_tujuan_pengiriman;
-        double harga, panjang, berat,tinggi, lebar,jarak;
+        double harga, panjang, berat, tinggi, lebar, jarak;
         
         System.out.print("input nama pengirim : ");
         nama_penerima = sc.nextLine();
@@ -23,5 +23,22 @@ public class sistemekspedisi {
         no_hp_penerima = sc.nextLine();
         System.out.print("input kota tujuan pengiriman : ");
         kota_tujuan_pengiriman = sc.nextLine();
+
+        String input;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Do you need insurance ?");
+        input = scan.next();
+
+        switch(input){
+            case "Yes":
+                System.out.println("Insurance will be processed immediately");
+                break;
+            default:
+                System.out.println(input + "anda tidak menggunakan asuransi" );
+        }
+
+
 }
 }
