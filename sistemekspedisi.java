50a2afceb761929a626f25af654425d3ec48c1a5
muathoillah <140579954+muathoillah@users.noreply.github.com>
2023-10-05 16:46:21 +0700

    komit

--- sistemekspedisi.java
+++ sistemekspedisi.java
@@ -1,7 +1,3 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
 import java.util.Scanner;
 
 public class sistemekspedisi {
@@ -9,8 +5,8 @@ public class sistemekspedisi {
         Scanner sc = new Scanner(System.in);
         
         String nama_penerima, alamat_penerima, no_telp_penerima, nama_pengirim, alamat_pengirim, no_telp_pengirim, asal_kota_pengiriman, kota_tujuan_pengiriman, input;
-        double harga, panjang, berat, tinggi, lebar, jarak;
-        boolean asuransi , member, cod, reguler, expres;
+        double harga, panjang, berat, tinggi, lebar, jarak, total, tarifper20km,harga2,harga3, nilai_barang;
+        boolean asuransi = true, member =true , cod = true ;
         
         System.out.print("input nama pengirim : ");
         nama_penerima = sc.nextLine();
@@ -28,8 +24,23 @@ public class sistemekspedisi {
         no_telp_penerima = sc.nextLine();
         System.out.print("input kota tujuan pengiriman : ");
         kota_tujuan_pengiriman = sc.nextLine();
+        System.out.println("nilai barang :");
+      //  nilai_barang = sc.nextLine();
+    
+        //about price
+        //if
+        tarifper20km= 15000;
+        harga = tarifper20km;
+        harga2 = harga*0.1 + (tarifper20km);
+        System.out.println( "harga" +harga);
+        
+        /*if (asuransi ) {
+            total = harga + (harga*0.02) + (nilai_barang * 0.01);
+        } else {
+            total = harga;
+        }*/
 
-        System.out.print("Do you need insurance?");
+    /*  System.out.print("Do you need insurance?");
         input = sc.next();
 
         switch(input){
@@ -48,13 +59,13 @@ public class sistemekspedisi {
         System.out.println("Do you have a member card?");
         input = sc.next();
 
-        switch(input){
+    /*  switch(input){
             case "Yes":
                 System.out.println("You get a shipping fee discount of 10.000");
                 break;
             default:
                 System.out.println("You do not get a discount on shipping costs");
-                LocalTime currentTime = LocalTime.now();
+            /*  LocalTime currentTime = LocalTime.now();
 
         // Get the current date
         LocalDate currentDate = LocalDate.now();
@@ -74,9 +85,8 @@ public class sistemekspedisi {
 
         // Get the current year
         int currentYear = currentDate.getYear();
-        System.out.println("Year: " + currentYear);
+        System.out.println("Year: " + currentYear); */
     
         }
 
-}
 }
